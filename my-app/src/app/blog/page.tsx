3718f6291b45bc2f84fbc055d7611b432a4ab62c
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-10-04 10:37:28 +0500

    add my-app to test next js app deployment on vercel

---
+++ my-app/src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Blog() {
+  return ( <>
+    <h1 className='h1'>Blog</h1>
+    <h2 className={`mb-3 text-2xl font-semibold`}>This is Blog Page</h2>
+    </>
+  )
+}
